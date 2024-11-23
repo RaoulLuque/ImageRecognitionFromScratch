dbd086c73625a1caf09eb01aeaf6fb6e79524ce2
RaoulLuque <raoulsluque@gmail.com>
2024-11-23 15:34:48 +0100

    Add epsilon for numerical computations copied from keras backend.

---
+++ src/config.py
@@ -0,0 +1,3 @@
+# Epsilon fuzz factor as used by keras backend, see
+# https://github.com/keras-team/keras/blob/ceabd61f89572a4b83c0348a96c5a8fd8af25b87/keras/src/backend/config.py#L9
+_EPSILON = 1e-7
