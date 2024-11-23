5703b432121ace7964e05ce6eb746010a2ab93d0
RaoulLuque <raoulsluque@gmail.com>
2024-11-23 13:32:06 +0100

    Add type annotations

--- src/layer.py
+++ src/layer.py
@@ -1,11 +1,13 @@
+import numpy as np
+
 # Base layer class
 class Layer:
     def __init__(self):
-        self.input = None
-        self.output = None
+        self.input: np.ndarray | None = None
+        self.output: np.ndarray | None = None
 
     # computes the output Y of a layer for a given input X
-    def forward_propagation(self, input):
+    def forward_propagation(self, input_data: np.ndarray) -> np.ndarray:
         raise NotImplementedError
 
     # computes dE/dX for a given dE/dY (and update parameters if any)
--- src/main.py
+++ src/main.py
@@ -2,6 +2,5 @@ def main():
     print("Hello from image-recognition-neural-network!")
 
 
-
 if __name__ == "__main__":
     main()
