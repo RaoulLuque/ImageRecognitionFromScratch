3d7ea2c43b81175d7c1a7d53fbf38884b8a88495
RaoulLuque <raoulsluque@gmail.com>
2024-11-24 23:35:03 +0100

    Change loss functions to be collected in enum for better typing

--- src/loss_function.py
+++ src/loss_function.py
@@ -1,5 +1,23 @@
 import numpy as np
 from nptyping import NDArray
+from enum import Enum
+
+
+class LossFunction(Enum):
+    """Enum class for loss functions."""
+    MEAN_SQUARED_ERROR = "mean_squared_error"
+
+    def function(self, y_true: NDArray, y_pred: NDArray) -> np.floating:
+        """Returns the loss function."""
+        match self:
+            case LossFunction.MEAN_SQUARED_ERROR:
+                return mean_squared_error(y_true, y_pred)
+
+    def derivative(self, y_true: NDArray, y_pred: NDArray) -> NDArray:
+        """Returns the derivative of the loss function."""
+        match self:
+            case LossFunction.MEAN_SQUARED_ERROR:
+                return mean_squared_error_derivative(y_true, y_pred)
 
 
 def mean_squared_error(y_true: NDArray, y_pred: NDArray) -> np.floating:
