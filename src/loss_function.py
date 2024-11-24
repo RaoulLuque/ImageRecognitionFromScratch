05c2361b6c6eee5e1213b986c3f9f583ffeb164b
RaoulLuque <raoulsluque@gmail.com>
2024-11-24 23:46:23 +0100

    Change name of mse loss function

--- src/loss_function.py
+++ src/loss_function.py
@@ -5,18 +5,18 @@ from enum import Enum
 
 class LossFunction(Enum):
     """Enum class for loss functions."""
-    MEAN_SQUARED_ERROR = "mean_squared_error"
+    mse = "mean_squared_error"
 
     def function(self, y_true: NDArray, y_pred: NDArray) -> np.floating:
         """Returns the loss function."""
         match self:
-            case LossFunction.MEAN_SQUARED_ERROR:
+            case LossFunction.mse:
                 return mean_squared_error(y_true, y_pred)
 
     def derivative(self, y_true: NDArray, y_pred: NDArray) -> NDArray:
         """Returns the derivative of the loss function."""
         match self:
-            case LossFunction.MEAN_SQUARED_ERROR:
+            case LossFunction.mse:
                 return mean_squared_error_derivative(y_true, y_pred)
 
 
