935b01b4453f150cc321e2d6b555ee1e25fc2baa
RaoulLuque <raoulsluque@gmail.com>
2024-11-24 23:47:41 +0100

    Add main function to create network and train on MNIST data. Use tensorflow for easier testing for now

--- src/main.py
+++ src/main.py
@@ -1,5 +1,41 @@
+import numpy as np
+from tensorflow.keras import datasets
+from tensorflow.keras import utils
+
+from src.activation_function import ActivationFunction
+from src.activation_layer import ActivationLayer
+from src.fully_connected_layer import FCLayer
+from src.loss_function import LossFunction
+from src.network import Network
+
+
 def main():
-    print("Hello from image-recognition-neural-network!")
+    # load MNIST dataset from server
+    (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+
+    # reshape and normalize input data
+    x_train = x_train.reshape(x_train.shape[0], 1, 28 * 28)
+    x_train = x_train.astype('float32')
+    x_train /= 255
+    y_train = utils.to_categorical(y_train)
+
+    x_test = x_test.reshape(x_test.shape[0], 1, 28 * 28)
+    x_test = x_test.astype('float32')
+    x_test /= 255
+    y_test = utils.to_categorical(y_test)
+
+    # Create neural network
+    model = Network()
+    model.add_layer(FCLayer(28 * 28, 100))  # input_shape=(1, 28*28)    ;   output_shape=(1, 100)
+    model.add_layer(ActivationLayer(ActivationFunction.tanh))
+    model.add_layer(FCLayer(100, 50))  # input_shape=(1, 100)      ;   output_shape=(1, 50)
+    model.add_layer(ActivationLayer(ActivationFunction.tanh))
+    model.add_layer(FCLayer(50, 10))  # input_shape=(1, 50)       ;   output_shape=(1, 10)
+    model.add_layer(ActivationLayer(ActivationFunction.tanh))
+
+    # Train only on part of the data since all of it would be pretty slow since batches are not implemented yet
+    model.set_loss_function(LossFunction.mse)
+    model.fit(x_train[0:1000], y_train[0:1000], epochs=35, learning_rate=0.1)
 
 
 if __name__ == "__main__":
