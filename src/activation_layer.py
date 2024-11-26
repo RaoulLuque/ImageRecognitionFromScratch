Fix backpropagation with mini-batches. Resolves #20

src/activation_layer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from layer import Layer
 from nptyping import NDArray
 
@@ -10,16 +12,23 @@ class ActivationLayer(Layer):
 
     Is used as intermediate layer to apply activation functions. The respective activation function is passed as a parameter on initialization.
     """
-    def __init__(self, activation_function: ActivationFunction):
+    def __init__(self, activation_function: ActivationFunction, number_of_neurons: int):
         super().__init__()
         self.activation_function = activation_function
+        self.number_of_neurons = number_of_neurons
 
-    def forward_propagation(self, input_data: NDArray) -> NDArray:
-        self.input = input_data
-        self.output = self.activation_function.function(self.input)
+    def forward_propagation(self, input_data: NDArray, size_current_batch: int, current_sample_index: int) -> NDArray:
+        if current_sample_index == 0:
+            # If batch has just started, initialize and reset input array to save input data for backpropagation at the end of the batch
+            self.input = np.zeros((size_current_batch, 1, self.number_of_neurons))
+        self.input[current_sample_index] = input_data
+        self.output = self.activation_function.function(self.input[current_sample_index])
         return self.output
 
-    # Returns input_error=dC/dZ for a given output_error=dC/dA.
+    # Returns input_error=dC/dZ for a given output_error=dC/dA by multiplying dA/dZ by dC/dA.
     # learning_rate is not used because there is no "learnable" parameters.
     def backward_propagation(self, output_error: NDArray, learning_rate: NDArray) -> NDArray:
-        return self.activation_function.derivative(self.input) * output_error
+        return np.multiply(self.activation_function.derivative(self.input), output_error)
+
+    def predict(self, input_data: NDArray) -> NDArray:
+        return self.activation_function.function(input_data)

src/fully_connected_layer.py
@@ -17,20 +17,28 @@ class FCLayer(Layer):
         super().__init__()
         self.weights: NDArray[Shape["input_size, output_size"], Any] = np.random.rand(input_size, output_size) - 0.5
         self.bias: NDArray[Shape["1, output_size"], Any] = np.random.rand(1, output_size) - 0.5
+        self.number_of_neurons = output_size
 
-    def forward_propagation(self, input_data: NDArray) -> NDArray:
-        self.input: NDArray[Shape["self.weights.shape[0]"], Any] = input_data
-        self.output: NDArray[Shape["self.weights.shape[1]"], Any] = np.dot(self.input, self.weights) + self.bias
+    def forward_propagation(self, input_data: NDArray, size_current_batch: int, current_sample_index: int) -> NDArray:
+        if current_sample_index == 0:
+            # If batch has just started, initialize and reset input array to save input data for backpropagation at the end of the batch
+            self.input = np.zeros((size_current_batch, 1, self.weights.shape[0]))
+        self.input[current_sample_index] = input_data
+        self.output: NDArray[Shape["self.number_of_neurons"], Any] = np.dot(self.input[current_sample_index], self.weights) + self.bias
         return self.output
 
     # computes dC/dW, dC/dB for a given output_error=dC/dZ. Returns input_error=dC/dA.
-    def backward_propagation(self, output_error: NDArray, learning_rate: NDArray) -> NDArray:
-        weights_error: NDArray = np.dot(self.input.T, output_error)
+    def backward_propagation(self, output_error_matrix: NDArray, learning_rate: NDArray) -> NDArray:
+        weights_error_matrix: NDArray = self.input.transpose(0, 2, 1) @ output_error_matrix
+        weights_error: NDArray = np.average(weights_error_matrix, axis=0)
 
         # Update weights and bias
-        self.weights -= learning_rate * weights_error
-        self.bias -= learning_rate * output_error
+        self.weights -= learning_rate * weights_error                           # subtract dC/dW from weights
+        self.bias -= learning_rate * np.average(output_error_matrix, axis=0)    # subtract dC/dB from bias
 
         # Compute error to propagate to previous layer (multiply dC/dZ by dZ/dA to obtain dC/dA)
-        input_error: NDArray = np.dot(output_error, self.weights.T)
+        input_error: NDArray = np.dot(output_error_matrix, self.weights.T)
         return input_error
+
+    def predict(self, input_data: NDArray) -> NDArray:
+        return np.dot(input_data, self.weights) + self.bias

src/network.py
@@ -47,7 +47,7 @@ class Network:
         for i in range(samples):
             output = input_data[i]
             for layer in self.layers:
-                output = layer.forward_propagation(output)
+                output = layer.predict(output)
             result.append(output)
 
         return result
@@ -76,19 +76,19 @@ class Network:
             # Error of the epoch to be displayed
             err = 0
             for j in range(len(x_train_batches)):
-                batch_error_to_propagate: NDArray = np.zeros((1, 10))
-                for k in range(len(x_train_batches[j])):
+                size_of_current_batch = len(x_train_batches[j])
+                # Initialize matrix to save vectors containing the error to propagate for each sample in the batch
+                # batch_error_to_propagate[i] contains the error for sample i in the batch
+                batch_error_to_propagate: NDArray = np.zeros((size_of_current_batch, 1, 10))
+                for k in range(size_of_current_batch):
                     # forward propagation
                     output = x_train_batches[j][k]
                     for layer in self.layers:
-                        output = layer.forward_propagation(output)
+                        output = layer.forward_propagation(output, size_of_current_batch, k)
 
                     # compute loss (for display purpose only)
                     err += self.loss_function.function(y_train_batches[j][k], output)
-                    batch_error_to_propagate += self.loss_function.derivative(y_train_batches[j][k], output)
-
-                # calculate average error for the batch
-                batch_error_to_propagate /= len(x_train_batches[j])
+                    batch_error_to_propagate[k] = self.loss_function.derivative(y_train_batches[j][k], output)
 
                 # backward propagation
                 for layer in reversed(self.layers):